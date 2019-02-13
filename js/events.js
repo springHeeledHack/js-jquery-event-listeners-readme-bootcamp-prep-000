becaab7e002e8e62d1981421436a8487666c9170
kyle smith <kylemclellan@gmail.com>
2019-02-13 16:44:03 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,13 +1,26 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!')
+  })
+}
+function frameIt(){
+  $('img').on('load', function(){
+    $(this).addClass('tasty')
+    $(this).css('border' 'solid 25px red')
+  })
+}
 function pressIt(){
-  $('window').on('keydown', function(e){
-    if (e.key === 'g'){
+  $('#typing').on('keydown', function(e){
+    if (e.which == 71){ 
       alert("You pressed the g key.")
-    }
+    } 
   })
 }
 $(document).ready(function(){
 
 // call functions here
+getIt()
+frameIt()
 pressIt()
 });
