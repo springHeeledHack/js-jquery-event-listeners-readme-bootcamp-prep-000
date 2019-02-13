94e2fd836d5baae8a407c11ad47c57c60cd2341f
kyle smith <kylemclellan@gmail.com>
2019-02-13 16:23:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,13 @@
 //define functions here
-
+function pressIt(){
+  $('window').on('keydown', function(e){
+    if (e.key === 'g'){
+      alert("You pressed the g key.")
+    }
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+pressIt()
 });
